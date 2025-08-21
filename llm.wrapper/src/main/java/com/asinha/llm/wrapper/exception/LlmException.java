bb3a08053278f19aa68146bd6fe54f711d583abd
Abhigyan Sinha <abhigyansinha2014@gmail.com>
2025-08-20 20:27:10 -0600

    [Add] Utils and Exceptions

---
+++ llm.wrapper/src/main/java/com/asinha/llm/wrapper/exception/LlmException.java
@@ -0,0 +1,7 @@
+package com.asinha.llm.wrapper.exception;
+
+public class LlmException extends Exception{
+    public LlmException(String errorMsg){
+        super(errorMsg);
+    }
+}
---
+++ llm.wrapper/src/main/java/com/asinha/llm/wrapper/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.asinha.llm.wrapper.utils;
+
+public class Constants {
+    public static final String OPENAI_API_URL = "https://api.openai.com/v1/chat/completions";
+    public static final String ANTHROPIC_API_URL = "https://api.anthropic.com/v1/complete";
+}
